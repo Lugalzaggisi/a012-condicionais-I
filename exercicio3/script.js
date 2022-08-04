e6ba57ba9251dbbe7e713b569661ee26e4b8702d
Lugalzaggisi <lugalzaggisi@gmail.com>
2022-08-04 00:28:13 -0300

    exercícios feitos

--- exercicio3/script.js
+++ exercicio3/script.js
@@ -1,1 +1,13 @@
-let nacionalidade //= adicione o prompt aqui
+let nacionalidade = prompt ("Escreva aqui sua nacionalidade") //= adicione o prompt aqui
+if(nacionalidade === "brasileira"){
+    console.log("é brasileiro")
+}else if(nacionalidade === "argentina"){
+    console.log("é argentino")
+}else if(nacionalidade === "uruguaia"){
+    console.log("é uruguaio")
+}else if(nacionalidade === "chilena"){
+    console.log("é chileno")
+}else if(nacionalidade === "colombiana"){
+    console.log("é colombiano")
+}else
+console.log("nacionalidade não encontrada")
